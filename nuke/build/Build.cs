6bc175050108a36ef9e07488b46b04c00cccc2a5
sebescudie <accounts@sebescudie.fr>
2025-03-13 22:22:01 +0100

    Prepares Nuke pipeline, not functional or even relevant for now

--- nuke/build/Build.cs
+++ nuke/build/Build.cs
@@ -1,5 +1,7 @@
 using System;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
@@ -8,21 +10,21 @@ using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    [Parameter("Version")]
+    readonly string Version = "1.0.0";
+
+    [Parameter("API Key for Chocolatey feed")]
+    readonly string ChocoApiKey;
+
+    [Parameter("Chocolatey feed URL")]
+    readonly string ChocoFeed;
 
     Target Clean => _ => _
         .Before(Restore)
@@ -35,10 +37,42 @@ class Build : NukeBuild
         {
         });
 
+    // Patch is built manually so we're leaving that empty
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+        });
+
+    // Create installer
+    Target BuildInstaller => _ => _
+        .Executes(() =>
+        {
+            var innoScriptTemplate = RootDirectory / "inno/installer.iss.template";
+            var innoScript = RootDirectory / "inno/installer.iss";
+
+            // Write version in template and render it
+            var content = File.ReadAllText(innoScriptTemplate);
+            content = content.Replace("##VERSION##", Version);
+            File.WriteAllText(innoScript, content);
+
+            var innoCompilerPath = "C:\\Program Files (x86)\\Inno Setup 6\\ISCC.exe";
+            var installerCompileProcess = ProcessTasks.StartProcess(innoCompilerPath, innoScript);
+            installerCompileProcess.WaitForExit();
+        });
+
+    // Create Chocolatey package
+    Target PackageChocolatey => _ => _
+        .Executes(() =>
+        {
+
+        });
+
+    // Publish Chocolatey package
+    Target PublishChocolatey => _ => _
+        .Executes(() =>
+        {
+
         });
 
 }
