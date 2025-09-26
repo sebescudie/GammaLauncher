720469b36541e863eadfbdb343f899e2077b7ad2
sebescudie <ping@sebescudie.fr>
2025-09-26 13:38:12 +0200

    Adds Utils static class in Nuke build

---
+++ nuke/build/Utils.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Build.Tasks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _build
+{
+    public static class Utils
+    {
+        public static void DeleteDirectoryContent(string directoryPath)
+        {
+            if (string.IsNullOrWhiteSpace(directoryPath))
+                throw new ArgumentException("Directory path cannot be null or empty.", nameof(directoryPath));
+
+            var directoryInfo = new DirectoryInfo(directoryPath);
+
+            if (!directoryInfo.Exists)
+                throw new DirectoryNotFoundException($"Directory not found: {directoryPath}");
+
+            // Delete all files
+            foreach (FileInfo file in directoryInfo.EnumerateFiles())
+            {
+                file.Delete();
+            }
+
+            // Delete all subdirectories
+            foreach (DirectoryInfo dir in directoryInfo.EnumerateDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+    }
+}
