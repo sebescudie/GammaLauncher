55748473814b61cfe97621815f43f5f94b7516a2
sebescudie <ping@sebescudie.fr>
2025-10-10 20:14:45 +0200

    Reworks paths, now have to manually pass compiler path, adds Github release creation and makes use Nuke tasks for choco pack and push

--- nuke/build/Build.cs
+++ nuke/build/Build.cs
@@ -1,66 +1,73 @@
+using _build;
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Chocolatey;
+using Nuke.Common.Utilities.Collections;
+using Octokit;
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
-using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
-using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using System.IO;
-using Nuke.Common.Utilities.Collections;
-using Nuke.Common.IO;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.PathConstruction;
-using System.Text.RegularExpressions;
-using System.Xml;
 using System.Xml.Linq;
-using _build;
 
 class Build : NukeBuild
 {
     public static int Main() => Execute<Build>(x => x.Clean);
 
-    const string TargetMagicString      = "##TARGET##";
-    const string VersionMagicString     = "##VERSION##";
-    const string winx64TargetString     = "win-x64";
-    const string winArm64TargetString   = "win-arm64";
+    // =======================================================
+    // PARAMS
+    // =======================================================
 
-    AbsolutePath innoCompilerPath       = "C:\\Program Files (x86)\\Inno Setup 6\\ISCC.exe";
-
-    AbsolutePath InnoFolder             = RootDirectory / .. / "inno";
-    AbsolutePath InnoScript             = RootDirectory / .. / "inno/installer.iss";
-
-    AbsolutePath NuspecFile             = RootDirectory / .. / "choco/GammaLauncher/gammalauncher.nuspec";
-    AbsolutePath ChocoToolsFolder       = RootDirectory / .. / "choco/GammaLauncher/tools";
-
-
-    AbsolutePath ArtifactsDirectory    = RootDirectory / .. / "artifacts";
-    AbsolutePath VvvvPropsFile          = RootDirectory / .. / "GammaLauncher.props";
-    AbsolutePath VvvvSourceFile         = RootDirectory / .. / "GammaLauncher.vl";
-
-    string Version                      = "";
-    AbsolutePath VersionFile            = RootDirectory / .. / "Version.props";
+    [Parameter("Compiler Path")]
+    readonly string CompilerPath;
 
     [Parameter("API Key for Chocolatey feed")]
+    [Secret]
     readonly string ApiKey;
 
     [Parameter("Chocolatey feed URL")]
     readonly string Feed;
+
+    // =======================================================
+    // PATHS AND MAGIC STRINGS
+    // =======================================================
+    
+    string Version = "";
+    
+    const string winX64Rid = "win-x64";
+    const string winArm64Rid = "win-arm64";
+
+    AbsolutePath innoCompilerPath = "C:\\Program Files (x86)\\Inno Setup 6\\ISCC.exe";
+
+    AbsolutePath InnoScript = RootDirectory / .. / "inno/installer.iss";
+
+    AbsolutePath NuspecFile = RootDirectory / .. / "choco/GammaLauncher/gammalauncher.nuspec";
+    AbsolutePath ChocoToolsFolder = RootDirectory / .. / "choco/GammaLauncher/tools";
+
+
+    AbsolutePath ArtifactsDirectory = RootDirectory / .. / "artifacts";
+    AbsolutePath VvvvPropsFile = RootDirectory / .. / "GammaLauncher.props";
+    AbsolutePath VvvvSourceFile = RootDirectory / .. / "GammaLauncher.vl";
+
+    AbsolutePath VersionFile = RootDirectory / .. / "Version.props";
+
+    // =======================================================
+    // RELEASE
+    // =======================================================
+
+    string GithubToken = Environment.GetEnvironmentVariable("GAMMALAUNCHER_GITHUB_TOKEN");
 
     Target Clean => _ => _
         .Executes(() =>
         {
             Console.WriteLine("Purging vvvv artifacts folder...");
             Utils.DeleteDirectoryContent(ArtifactsDirectory);
-            
+
             // Delete installer from inno and choco/tools folders
             // We search in both folder in case something got wrong during previous run and the installer
             // was not moved to /tools
             var exeInTools = Directory.EnumerateFiles(ChocoToolsFolder).FirstOrDefault(f => Path.GetFileName(f).EndsWith("exe"));
-            if(!exeInTools.IsNullOrEmpty())
+            if (!exeInTools.IsNullOrEmpty())
             {
                 Console.WriteLine("Deleting outdated exe from Choco /tools folder");
                 File.Delete(exeInTools);
@@ -88,53 +95,39 @@ class Build : NukeBuild
         .DependsOn(GetVersion)
         .Executes(() =>
         {
-            // Parse GammaLauncher entry point to find the vvvversion it was saved with
-            // and infer compiler path from that
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.LoadXml(File.ReadAllText(VvvvSourceFile));
-            var xmlVersion = xmlDoc.SelectSingleNode("//Document/@LanguageVersion").Value ?? throw new InvalidDataException("Could not parse vvvv source file");
-
-            // For now we're hardcoding the architecture but should find something more elegant at some point
-            var vvvvFolderName = $"vvvv_gamma_{xmlVersion.Remove(0, 5)}-win-x64";
-            var compilerPath = Path.Combine("C:\\Program Files\\vvvv", vvvvFolderName,"vvvvc.exe");
-            Console.WriteLine($"GammaLauncher was last saved with {xmlVersion}");
-            Console.WriteLine($"Expecting to find compiler in {compilerPath}");
-            if(File.Exists(compilerPath))
-                Console.WriteLine("Found corresponding CLI compiler");
-
             // Set rid to win-x64
             var launcherPropsXdoc = XDocument.Load(VvvvPropsFile);
 
-            launcherPropsXdoc.Descendants(XName.Get("RuntimeIdentifier", "http://schemas.microsoft.com/developer/msbuild/2003")).First().Value = winx64TargetString;
+            launcherPropsXdoc.Descendants(XName.Get("RuntimeIdentifier", "http://schemas.microsoft.com/developer/msbuild/2003")).First().Value = winX64Rid;
             launcherPropsXdoc.Save(VvvvPropsFile);
 
             // Compile win-x64
-            var buildWinx64 = ProcessTasks.StartProcess(compilerPath, $"{VvvvSourceFile} --output-type WinExe --rid win-x64 --clean");
+            var buildWinx64 = ProcessTasks.StartProcess(CompilerPath, $"{VvvvSourceFile} --output-type WinExe --rid {winX64Rid} --clean");
             buildWinx64.WaitForExit();
 
             // Set rid to win-arm64
-            launcherPropsXdoc.Descendants(XName.Get("RuntimeIdentifier", "http://schemas.microsoft.com/developer/msbuild/2003")).First().Value = winArm64TargetString;
+            launcherPropsXdoc.Descendants(XName.Get("RuntimeIdentifier", "http://schemas.microsoft.com/developer/msbuild/2003")).First().Value = winArm64Rid;
             launcherPropsXdoc.Save(VvvvPropsFile);
 
             // Compile win-arm
-            var buildWinArm = ProcessTasks.StartProcess(compilerPath, $"{VvvvSourceFile} --output-type WinExe --rid win-arm64 --clean");
+            var buildWinArm = ProcessTasks.StartProcess(CompilerPath, $"{VvvvSourceFile} --output-type WinExe --rid {winArm64Rid} --clean");
             buildWinArm.WaitForExit();
 
             // Delete src folders
-            var winx64SrcFolder = ArtifactsDirectory / winx64TargetString / "src";
-            if(Directory.Exists(winx64SrcFolder))
+            var winx64SrcFolder = ArtifactsDirectory / winX64Rid / "src";
+            if (Directory.Exists(winx64SrcFolder))
                 Directory.Delete(winx64SrcFolder, true);
 
-            var winarm64SrcFolder = ArtifactsDirectory / winArm64TargetString / "src";
-            if(Directory.Exists(winarm64SrcFolder))
+            var winarm64SrcFolder = ArtifactsDirectory / winArm64Rid / "src";
+            if (Directory.Exists(winarm64SrcFolder))
                 Directory.Delete(winarm64SrcFolder, true);
 
             // Create portable zips
-            var winx64BuildOutput = ArtifactsDirectory / winx64TargetString;
-            winx64BuildOutput.ZipTo(ArtifactsDirectory / $"gammalauncher_{Version}_win-x64_portable.zip");
+            var winx64BuildOutput = ArtifactsDirectory / winX64Rid;
+            winx64BuildOutput.ZipTo(ArtifactsDirectory / $"gammalauncher_{Version}_{winX64Rid}_portable.zip");
 
-            var winarm64BuildOutput = ArtifactsDirectory / winArm64TargetString;
-            winarm64BuildOutput.ZipTo(ArtifactsDirectory / $"gammalauncher_{Version}_win-arm64_portable.zip");
+            var winarm64BuildOutput = ArtifactsDirectory / winArm64Rid;
+            winarm64BuildOutput.ZipTo(ArtifactsDirectory / $"gammalauncher_{Version}_{winArm64Rid}_portable.zip");
         });
 
     // Create installer
@@ -143,51 +136,69 @@ class Build : NukeBuild
         .Executes(() =>
         {
             // Build winx64 installer
-            var winX64installerCompileProcess = ProcessTasks.StartProcess(innoCompilerPath, $"/DMyAppVersion={Version} /DMyTarget={winx64TargetString} {InnoScript}");
+            var winX64installerCompileProcess = ProcessTasks.StartProcess(innoCompilerPath, $"/DMyAppVersion={Version} /DMyTarget={winX64Rid} {InnoScript}");
             winX64installerCompileProcess.WaitForExit();
 
             // Build arm64 installer
-            var Arm64installerCompileProcess = ProcessTasks.StartProcess(innoCompilerPath, $"/DMyAppVersion={Version} /DMyTarget={winArm64TargetString} {InnoScript}");
+            var Arm64installerCompileProcess = ProcessTasks.StartProcess(innoCompilerPath, $"/DMyAppVersion={Version} /DMyTarget={winArm64Rid} {InnoScript}");
             Arm64installerCompileProcess.WaitForExit();
         });
 
+    // Create Github release
+    Target CreateGithubRelease => _ => _
+        .Executes(async () =>
+        {
+            // Create release
+            GitHubClient client = new GitHubClient(new ProductHeaderValue("gammalauncher.nuke"));
+            client.Credentials = new Credentials(GithubToken);
+
+            var release = new NewRelease(Version);
+            release.Name = Version;
+            release.Body = "";
+
+            var result = await client.Repository.Release.Create("sebescudie", "GammaLauncher", release);
+      
+            // Upload artifacts
+            var lastRelease = await client.Repository.Release.GetLatest("sebescudie", "GammaLauncher");
+
+            var artifacts = ArtifactsDirectory.GlobFiles("*.zip", "*.exe");
+
+            foreach(var artifact in artifacts)
+            {
+                var releaseAsset = new ReleaseAssetUpload
+                {
+                    FileName = artifact.Name,
+                    RawData = File.OpenRead(artifact)
+                };
+            }
+        });
+
     // Create Chocolatey package
-    Target Pack => _ => _
-        .DependsOn(GetVersion)
-        .DependsOn(BuildInstaller)
+    Target PackChocolatey => _ => _
+        .DependsOn(CreateGithubRelease)
         .Executes(() =>
         {
-            var nuspecXDoc = XDocument.Load(NuspecFile);
-            nuspecXDoc.Descendants(XName.Get("version", "http://schemas.microsoft.com/packaging/2015/06/nuspec.xsd")).FirstOrDefault().Value = Version;
-            nuspecXDoc.Save(NuspecFile);
-
-            AbsolutePath installerPath = ArtifactsDirectory / $"gammalauncher_{Version}_{winx64TargetString}_installer.exe";
-            var installerTargetPath = $"{ChocoToolsFolder}/{Path.GetFileName(installerPath)}";
+            // Move installer to /tools
+            AbsolutePath installerPath = ArtifactsDirectory / $"gammalauncher_{Version}_{winX64Rid}_installer.exe";
+            AbsolutePath installerTargetPath = $"{ChocoToolsFolder}/{Path.GetFileName(installerPath)}";
             File.Copy(installerPath, installerTargetPath);
 
             // Pack
-            var packProcess = ProcessTasks.StartProcess("choco", $"pack {NuspecFile}", ArtifactsDirectory);
-            packProcess.WaitForExit();
+            ChocolateyTasks.ChocolateyPack(settings => settings
+                .SetPathToNuspec(installerTargetPath)
+                .SetOutputDirectory(ArtifactsDirectory)
+                .SetVersion(Version));
         });
 
     // Publish Chocolatey package
     Target Publish => _ => _
-        .DependsOn(Pack)
+        .DependsOn(PackChocolatey)
         .Requires(() => !string.IsNullOrWhiteSpace(Feed))
         .Requires(() => !string.IsNullOrWhiteSpace(ApiKey))
         .Executes(() =>
         {
-            // Locate nupkg, which should be in nuke's RootDirectory
-            var nupkg = Directory.EnumerateFiles(RootDirectory).FirstOrDefault(f => Path.GetExtension(f) == ".nupkg");
-            Console.WriteLine($"Nupkg: {nupkg}");
-            if (!nupkg.IsNullOrEmpty())
-            {
-                var pushProcess = ProcessTasks.StartProcess("choco", $"push {nupkg} --source {Feed} --api-key {ApiKey}");
-                pushProcess.WaitForExit();
-            }
-            else
-            {
-                Console.WriteLine("Could not file nupkg");
-            }
+            ChocolateyTasks.ChocolateyPush(settings => settings
+                .SetProcessWorkingDirectory(ArtifactsDirectory)
+                .SetApiKey(ApiKey));
         });
 }
