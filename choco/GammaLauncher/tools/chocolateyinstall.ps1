9032fd81f37f84420e43f3f617186b2c1149adaa
sebescudie <ping@sebescudie.fr>
2025-11-19 12:18:31 -0800

    Updates generated ChocolateyInstall script

--- choco/GammaLauncher/tools/chocolateyinstall.ps1
+++ choco/GammaLauncher/tools/chocolateyinstall.ps1
@@ -1,15 +1,13 @@
-﻿$ErrorActionPreference = 'Stop'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$installer = Get-ChildItem -Path $folder -Filter "*.exe" | Select-Object -First 1 -ExpandProperty FullName
+$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'EXE'
-  file          = Join-Path $toolsDir $installer
-  softwareName  = 'GammaLauncher*'
-  checksumType  = 'sha256'
-  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  validExitCodes= @(0)
+packageName    = 'gammalauncher'
+fileType       = 'exe'
+url64bit       = 'https://github.com/sebescudie/GammaLauncher/releases/download/5.3.2/gammalauncher_5.3.2_win-x64_installer.exe'
+checksum64     = '39e3bd87a8f87415b0e7218cba32c6a90cabdcb431105009d4fcdab2dcecf504'
+checksumType64 = 'sha256'
+silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+validExitCodes = @(0)
 }
 
-Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyPackage @packageArgs
